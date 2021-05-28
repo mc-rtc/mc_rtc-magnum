9f6ca643eb24c23da07b590e99a8434c8a19877b
Pierre Gergondet <pierre.gergondet@gmail.com>
2021-05-28 15:06:44 +0800

    Replace get_current_dir_name

--- src/widgets/Schema.cpp
+++ src/widgets/Schema.cpp
@@ -9,9 +9,15 @@ namespace details
 
 inline bfs::path current_path()
 {
-  auto cwd = get_current_dir_name();
+  std::vector<char> buffer;
+  buffer.reserve(2048);
+  auto cwd = getcwd(buffer.data(), buffer.capacity());
+  while(cwd == nullptr)
+  {
+    buffer.reserve(2 * buffer.capacity());
+    cwd = getcwd(buffer.data(), buffer.capacity());
+  }
   bfs::path out(cwd);
-  free(cwd);
   return out;
 }
 
