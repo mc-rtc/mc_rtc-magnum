e9403bde67ff3ba131324dd522bf8f4ad075470c
Arnaud TANGUY <arn.tanguy@gmail.com>
2026-04-16 17:44:58 +0200

    nix(main): override to use standalone version

--- flake.nix
+++ flake.nix
@@ -2,14 +2,15 @@
   description = "mc-rtc-magnum standalone gui visualization";
 
   inputs.mc-rtc-nix.url = "github:mc-rtc/nixpkgs";
+  inputs.mc-rtc-magnum-standalone.url = "github:mc-rtc/mc_rtc-magnum/nix";
 
   outputs =
     inputs:
     inputs.mc-rtc-nix.lib.mkFlakoboros inputs (
-      { lib, ... }:
+      { ... }:
       {
         overrideAttrs.mc-rtc-magnum = {
-          src = lib.cleanSource ./.;
+          src = builtins.warn "warning: mc-rtc-magnum on main branch does not support nix, running from the latest locked commit in nix branch" inputs.mc-rtc-magnum-standalone;
         };
       }
     );
