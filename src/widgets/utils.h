a184a5b6c2886c4a3e24619887c8dba49df5ac3e
Arnaud TANGUY <arn.tanguy@gmail.com>
2026-06-20 17:27:43 +0200

    feat: use mc_rtc::convertURI, fixes non-ros display

--- src/widgets/utils.h
+++ src/widgets/utils.h
@@ -74,42 +74,6 @@ inline sva::PTransformd convert(const Magnum::Matrix4 & m)
   return {r_map.cast<double>(), t_map.cast<double>()};
 }
 
-inline fs::path convertURI(const std::string & uri, [[maybe_unused]] std::string_view default_dir = "")
-{
-  const std::string package = "package://";
-  if(uri.size() >= package.size() && uri.find(package) == 0)
-  {
-    size_t split = uri.find('/', package.size());
-    std::string pkg = uri.substr(package.size(), split - package.size());
-    auto leaf = fs::path(uri.substr(split + 1));
-    fs::path MC_ENV_DESCRIPTION_PATH(mc_rtc::MC_ENV_DESCRIPTION_PATH);
-#ifndef __EMSCRIPTEN__
-#  ifndef MC_RTC_HAS_ROS_SUPPORT
-    // FIXME Prompt the user for unknown packages
-    if(pkg == "jvrc_description") { pkg = (MC_ENV_DESCRIPTION_PATH / ".." / "jvrc_description").string(); }
-    else if(pkg == "mc_env_description") { pkg = MC_ENV_DESCRIPTION_PATH.string(); }
-    else if(pkg == "mc_int_obj_description")
-    {
-      pkg = (MC_ENV_DESCRIPTION_PATH / ".." / "mc_int_obj_description").string();
-    }
-    else { pkg = default_dir; }
-#  else
-#    ifdef MC_RTC_ROS_IS_ROS2
-    pkg = ament_index_cpp::get_package_share_directory(pkg);
-#    else
-    pkg = ros::package::getPath(pkg);
-#    endif
-#  endif
-#else
-    pkg = "/assets/" + pkg;
-#endif
-    return pkg / leaf;
-  }
-  const std::string file = "file://";
-  if(uri.size() >= file.size() && uri.find(file) == 0) { return fs::path(uri.substr(file.size())); }
-  return uri;
-}
-
 inline Magnum::Color4 color(const rbd::parsers::Material & m)
 {
   if(m.type == rbd::parsers::Material::Type::COLOR)
